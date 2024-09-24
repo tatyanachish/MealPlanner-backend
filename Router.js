8f135cb6c8773c4ff915891007b4176e4d96a0a5
Tatiana Chishkina <tatyanachishkina@gmail.com>
2024-09-23 21:07:22 -0400

    Update Router.js
    replace delete on post

--- Router.js
+++ Router.js
@@ -5,7 +5,7 @@ const { getMeal, saveMeals, deleteMeal, editMeal} = require('./MealController');
 
 router.get('/',getMeal);
 router.post('/saveMeals',saveMeals);
-router.delete('/deleteMeal',deleteMeal);
+router.post('/deleteMeal',deleteMeal);
 router.put('/editMeal',editMeal)
 
 
